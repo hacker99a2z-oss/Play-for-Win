cd4abf6fd22b91f5cd1de9793028a08b60c5b545
hacker99a2z-oss <hacker99a2z@gmail.com>
2026-08-10 19:54:59 +0600

    Update Adsgram blockId in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -86,7 +86,7 @@ export default function App() {
     return new Promise((resolve) => {
       if (window.Adsgram) {
         const AdController = window.Adsgram.init({
-          blockId: "int-41387"
+          blockId: "41655"
         });
 
         AdController.show()
