f24347f2a5f01ca560466f3970a3f20a20851228
hacker99a2z-oss <hacker99a2z@gmail.com>
2026-08-12 21:43:08 +0600

    Change hole appearance timing in Home.jsx
    Adjusted timing for new holes appearance in the game.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -69,11 +69,11 @@ const Home = ({ user, onPlayAd, refreshUserData }) => {
               }
               return updated;
             });
-          }, 700);
+          }, 600);
 
           return newHoles;
         });
-      }, 1000); // প্রতি ০.৭ সেকেন্ড পর পর নতুন ইঁদুর বের হবে
+      }, 1100); // প্রতি ০.৭ সেকেন্ড পর পর নতুন ইঁদুর বের হবে
     } else {
       setHoles(Array(16).fill(null));
     }
