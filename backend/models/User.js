74e8ab651091d4b627036089141136085576f97a
hacker99a2z-oss <hacker99a2z@gmail.com>
2026-08-13 18:59:44 +0600

    Fix indentation in User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
   gamesPlayedForReferral: { type: Number, default: 0 }, // 👈 গেম খেলার কাউন্টার
   referralBonusGiven: { type: Boolean, default: false },
   country: { type: String, default: 'Unknown' },
-  isVpn: { type: Boolean, default: false }
+  isVpn: { type: Boolean, default: false },
 
   createdAt: { type: Date, default: Date.now }
 });
