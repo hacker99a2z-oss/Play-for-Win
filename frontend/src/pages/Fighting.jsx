9739de1cc9460cd0c49489e5085b6ed079926dbb
hacker99a2z-oss <hacker99a2z@gmail.com>
2026-08-16 19:51:11 +0600

    Update position of power indicator in Fighting.jsx
    Adjusted the position of the power indicator.

--- frontend/src/pages/Fighting.jsx
+++ frontend/src/pages/Fighting.jsx
@@ -58,8 +58,8 @@ const Fighting = ({ onPlayAd }) => {
               <div 
                 className="absolute font-black text-[10px] sm:text-xs tracking-wider"
                 style={{
-                  bottom: '5%',
-                  left: '50%',
+                  bottom: '3%',
+                  left: '52%',
                   color: mouse.color,
                   textShadow: '0px 2px 4px rgba(0,0,0,0.95)'
                 }}
