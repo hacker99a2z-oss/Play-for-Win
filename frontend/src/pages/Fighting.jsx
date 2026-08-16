83f844927f939553e544e21cec5f600e1aa1c746
hacker99a2z-oss <hacker99a2z@gmail.com>
2026-08-16 20:49:55 +0600

    Update card position in Fighting component
    Adjusted the vertical position of the card for better alignment.

--- frontend/src/pages/Fighting.jsx
+++ frontend/src/pages/Fighting.jsx
@@ -28,7 +28,7 @@ const Fighting = ({ onPlayAd }) => {
       img: mouseGamma,
       // 🟢 Gamma (সবুজ) টেক্সটের পজিশন
       textPos: { bottom: '6%', left: '46.6%' },
-      cardPos: 'translate-y-0.7 translate-x-1'
+      cardPos: 'translate-y-0.8 translate-x-1'
     },
   ]);
 
